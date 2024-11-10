703ff8ce7da7056d9be166dc689494df7b148587
fthe <lcslianandfthe.cs@gmail.com>
2024-11-10 22:25:37 +0900

    feature: added delta time with 60 fps limit

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ from constants import *
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode(
-        (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(pygame.Color(0, 0, 0))
         pygame.display.flip()
+
+        # limit the framerate to 60FPS
+        dt = clock.tick(60.0)/1000
 
 
 if __name__ == "__main__":
