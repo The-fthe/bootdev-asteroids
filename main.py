61b6d7681b92524deb77be5be638987655d11b4d
fthe <lcslianandfthe.cs@gmail.com>
2024-11-10 23:41:10 +0900

    feature: added player and triangle drawing, with not working exit q and CTRL C escape

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import pygame
+import player
+import signal
+import sys
 from constants import *
+
+
+def signal_handler(sig, frame):
+    pygame.quit()
+    sys.exit(0)
 
 
 def main():
     pygame.init()
+    signal.signal(signal.SIGTERM, signal_handler)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     clock = pygame.time.Clock()
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    ply = player.Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
                 return
+            elif event.type == pygame.KEYDOWN:
+                # Add Q key handling
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    sys.exit()
+
         screen.fill(pygame.Color(0, 0, 0))
+        ply.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60FPS
